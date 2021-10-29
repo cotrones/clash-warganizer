3d0979a8400512bacd929be22b3e27a31e2b7c29
cotrones <tomcotroneo@gmail.com>
2021-10-29 15:23:29 -0400

    add apollo-server-express and test apollo studio sandbox with hello resolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,24 +4,45 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
-const app = express();
-const port = process.env.PORT;
+const main = async () => {
+  const app = express();
+  const port = process.env.PORT;
 
-app.use(morgan("dev"));
-app.use(helmet());
-app.use(
-  cors({
-    // origin: "*",
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+  app.use(morgan("dev"));
+  app.use(
+    helmet({
+      // needed to use apollo graphql studio sandbox in dev
+      contentSecurityPolicy:
+        process.env.NODE_ENV === "production" ? undefined : false,
+    })
+  );
+  app.use(
+    cors({
+      origin: "*", // needed to use apollo graphql studio sandbox in dev
+      // origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
 
-app.get("/", (_req, res) => {
-  res.send("<h2>Hello world</h2>");
-});
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+    }),
+  });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
 });
